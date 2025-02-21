cc3abf8b0cf2cd4c411e2e19a6a8358dbe63dd6d
Exootik44 <sbillet@gaming.tech>
2025-02-21 13:38:25 +0100

    [ADD] Correction texture fonctionnelle

--- lyo-t2-atari-p7-07/src/MoteurDX/InitDirect3DApp.cpp
+++ lyo-t2-atari-p7-07/src/MoteurDX/InitDirect3DApp.cpp
@@ -67,7 +67,7 @@ bool InitDirect3DApp::Initialize()
 	hr = DirectX::CreateDDSTextureFromFile12(
 		mD3DDevice.Get(),
 		mCommandList.Get(),
-		L"../../../src/MoteurDX/WoodCrate02.dds",
+		L"../../../src/MoteurDX/tile.dds",
 		m_Texture,
 		m_TextureUploadHeap);
 	if (FAILED(hr))
@@ -318,7 +318,7 @@ void InitDirect3DApp::CreatePipelineState()
 	D3D12_INPUT_ELEMENT_DESC inputLayout[] = {
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
 		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
-		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 24, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 28, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
 	};
 
 	// Create the pipeline state object
